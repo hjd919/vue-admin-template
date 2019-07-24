b7e0dc18b225aeafb6e45efa174869555eed106a
hjd919 <hjd919@126.com>
2019-07-24 19:22:31 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -55,6 +55,18 @@ export const constantRoutes = [
     }]
   },
 
+  /*  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
+  },
+
   {
     path: '/example',
     component: Layout,
@@ -158,7 +170,7 @@ export const constantRoutes = [
       }
     ]
   },
-
+ */
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
